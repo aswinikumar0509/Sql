69716307b1c66956b13402d2ff72a3d18cee3d2a
Aswini Kumar <64352951+aswinikumar0509@users.noreply.github.com>
2022-07-31 21:47:09 +0530

    Add files via upload

---
+++ sql10.sql
@@ -0,0 +1,15 @@
+create database abcd;
+
+use abcd;
+
+create table  customers(
+CustomerID int,
+CustomerName varchar(50),
+ContactName varchar(50),
+Address varchar(50),
+City varchar(50),
+PostalCode int,
+Country varchar(50)
+);
+
+select * from customers;
